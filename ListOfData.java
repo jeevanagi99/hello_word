02a81521c99eda39e4d3db8d0338db2f34f9082a
jeevanagi99 <89789326+jeevanagi99@users.noreply.github.com>
2021-09-02 15:15:25 +0530

    initial commit

---
+++ ListOfData.java
@@ -0,0 +1,14 @@
+class ListOfData{
+public static void main(String args[]){
+byte currentMonth=8;
+short boatHeadPhonePrice=3000;
+int arpitaSalary=75000;
+long phoneNumber=123456789L;
+double pi=3.14141414;
+System.out.println("value of byte:"+currentMonth);
+System.out.println("value of byt:"+ boatHeadPhonePrice);
+System.out.println("value of byte:"+arpitaSalary);
+System.out.println("value of byte:"+phoneNumber);
+System.out.println("value of byte:"+pi);
+}
+}
